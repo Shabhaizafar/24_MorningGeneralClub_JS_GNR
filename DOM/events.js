94dbd218a0c7f8f1f1f7ba9e9f3aa4a7eb523d75
Shabhai Zafar <zafarshabhai03@gmail.com>
2024-09-25 08:53:59 +0530

    Events

--- DOM/events.js
+++ DOM/events.js
@@ -1,4 +1,13 @@
 const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+const btn3 = document.querySelector('.btn3');
+const btn4 = document.querySelector('.btn4');
+const btn8 = document.querySelector('.btn8');
+const btn5 = document.querySelector('.btn5');
+const btn6 = document.querySelector('.btn6');
+
+
+
 
 
 btn1.onclick = function (){
@@ -6,3 +15,45 @@ btn1.onclick = function (){
     btn1.classList.add("animation");
     console.log(btn1.classList);
 }
+
+//Anonymous
+btn2.onmouseover = function (){
+    document.body.style.backgroundColor = 'red';
+}
+
+btn3.onmouseleave = function (){
+    document.body.style.backgroundColor = 'blue';
+}
+
+
+btn4.onmousedown = function (){
+    document.body.style.backgroundColor = 'yellow';
+}
+
+btn8.onmouseup = function (){
+    document.body.style.backgroundColor = 'lime';
+}
+
+btn5.onmouseenter = function (){
+    document.body.style.backgroundColor = 'purple';
+}
+
+btn6.onmousemove = function (){
+    document.body.style.backgroundColor = 'gray';
+}
+
+
+
+/////////////////////
+// const div = document.querySelector('div');
+// div.onmouseenter = function (){
+//     div.style.backgroundColor = "navy";
+    
+// }
+
+
+// overmouseover vs onmouseenter
+
+// overmouseleave vs onmouseout
+
+// overmousewheel 
