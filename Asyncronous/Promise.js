bace9b5e1bd44430728e0eb3d672d27c45e5a222
Shabhaizafar <zafarshabhai03@gmail.com>
2024-11-26 10:52:00 +0530

    Promise

---
+++ Asyncronous/Promise.js
@@ -0,0 +1,49 @@
+// console.log("Hello Eveyone");
+
+// setTimeout(() => {
+//     console.log("Set Timer !!!");
+// }, 100);
+// //.5s
+// for (let i = 0; i < 1000; i++) {
+//     console.log(1);
+// }
+// console.log("Program End !!!");
+
+
+
+
+// // syncronous : JS
+// // asyncronous
+
+// console.log(document.querySelector('h1').innerHTML);
+
+// var n = 1;
+// if(n<10){
+//     fu1();
+// }else{
+//     fu2();
+// }
+
+
+// function fu1(){
+//     console.log("Fu1");
+// }
+// function fu2(){
+//     console.log("Fu2");
+// }
+
+// var data;
+// var mypromise = new Promise((success,reject)=>{
+//     if(-1){
+//         success();
+//     }else{
+//         reject();
+//     }
+// });
+
+// mypromise.then(()=>{
+//     console.log("Success !!");
+// },
+// ()=>{
+//     console.log("Reject !!");
+// });
