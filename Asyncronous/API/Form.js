a0e4b3c59640d1f605a3b897cf6a70baae0b4035
Shabhaizafar <zafarshabhai03@gmail.com>
2024-12-23 10:30:39 +0530

    History API

--- Asyncronous/API/Form.js
+++ Asyncronous/API/Form.js
@@ -1,12 +1,23 @@
-const btn = document.querySelector('button');
-const input = document.querySelector('input');
+// const btn = document.querySelector('button');
+// const input = document.querySelector('input');
 
-btn.addEventListener('click',function(event){
-    event.preventDefault();
-    if(!input.checkValidity()){
-        document.querySelector('span').innerHTML = input.validationMessage;
-    }else{
-        document.querySelector('span').innerHTML = "Marks Correct";
-    }
+// btn.addEventListener('click',function(event){
+//     event.preventDefault();
+//     if(!input.checkValidity()){
+//         document.querySelector('span').innerHTML = input.validationMessage;
+//     }else{
+//         document.querySelector('span').innerHTML = "Marks Correct";
+//     }
+// });
 
-});
+// const btn = document.querySelector('button');
+
+
+// btn.addEventListener('click',function(){
+    //next 
+    // history.forward();
+
+    // history.back();
+
+    // history.go(-1);
+// });
